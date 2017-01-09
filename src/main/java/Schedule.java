21d8a7cd8299457a2f66f9e5b15f4fb4e8bd6e93
Ryan Ly <thinkbeam@gmail.com>
2017-01-09 15:20:54 -0500

    Began support for Classroom API

--- src/main/java/Schedule.java
+++ src/main/java/Schedule.java
@@ -26,4 +26,29 @@ public abstract class Schedule {
     public String getName(){
         return this.name;
     }
+
+    public static boolean checkInput(String input){
+        if(!input.contains(":")){
+            return false;
+        }
+        int colonIndex = -1;
+        for(int i = 0;i < input.length();i++){
+            if(input.charAt(i) == ':'){
+                colonIndex = i;
+            }
+        }
+
+        try{
+            int hour = Integer.parseInt(input.substring(0,colonIndex));
+            int minute = Integer.parseInt(input.substring(colonIndex+1,input.length()));
+
+            if(hour < 0 || hour >= 24 || minute < 0 || minute >= 60){
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e){
+            return false;
+        }
+    }
 }
